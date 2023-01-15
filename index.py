9e0b8f0b893c0bd23fc243bdb5c048427202ac03
Mubeen31 <qs6272527@gmail.com>
2023-01-15 19:47:55 +0000

    first commit

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ from dash import dcc
 from dash.dependencies import Output, Input
 import pandas as pd
 from datetime import datetime
+import dash_bootstrap_components as dbc
 
 metaTags = [
     {'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5'}]
@@ -32,7 +33,7 @@ app.layout = html.Div([
                 html.Div('Walsall, England', className='location_name')
             ], className='location_row'),
 
-            html.Div(id='data_update_time')
+            dbc.Spinner(html.Div(id='data_update_time'))
 
         ], className='title_background twelve columns')
     ], className='row'),
