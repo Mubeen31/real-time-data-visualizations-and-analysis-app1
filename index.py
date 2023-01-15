60332871201e7cf8c2b1c70dcf54d18b68997795
Mubeen31 <qs6272527@gmail.com>
2023-01-15 19:23:55 +0000

    first commit

--- index.py
+++ index.py
@@ -9,6 +9,7 @@ metaTags = [
     {'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5'}]
 
 app = dash.Dash(__name__, meta_tags=metaTags)
+server = app.server
 
 app.layout = html.Div([
 
